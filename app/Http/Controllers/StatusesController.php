38f19b9da3fd0e42e93bb5978c39c2f2daacb8b3
switch <switch_zhou@163.com>
2020-06-11 06:05:06 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,7 +27,11 @@ class StatusesController extends Controller
 
     }
 
-    public function destroy(){
-
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy',$status);
+        $status->delete();
+        session()->flash('success','微博已被成功删除！');
+        return redirect()->back();
     }
 }
